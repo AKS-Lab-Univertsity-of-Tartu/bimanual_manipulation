Fixed tray rotation. Tray pnp ok.

real_demo/real_demo/dual_arm_demo.py
@@ -151,7 +151,7 @@ class Planner(Node):
         model_path = os.path.join(get_package_share_directory('real_demo'), 'ur5e_hande_mjx', 'scene.xml')
         self.model = mujoco.MjModel.from_xml_path(model_path)
         self.model.opt.timestep = timestep
-        self.tray_idx = self.model.jnt_qposadr[self.model.body_jntadr[self.model.body(name="tray").id]]
+        # self.tray_idx = self.model.jnt_qposadr[self.model.body_jntadr[self.model.body(name="tray").id]]
 
         target_0_rot = quaternion_multiply(quaternion_multiply(self.model.body(name="target_0").quat, rotation_quaternion(-180, [0, 1, 0])), rotation_quaternion(-90, [0, 0, 1]))
         target_1_rot = quaternion_multiply(quaternion_multiply(self.model.body(name="target_1").quat, rotation_quaternion(180, [0, 1, 0])), rotation_quaternion(90, [0, 0, 1]))
@@ -249,13 +249,15 @@ class Planner(Node):
             self.data.mocap_pos[self.model.body_mocapid[self.model.body(name='tray_mocap').id]] = tray_pos
 
             tray_rot_init = self.data.mocap_quat[self.model.body_mocapid[self.model.body(name='tray_mocap').id]]
-            tray_rot = turn_quat(self.eef_pos_1_init, self.eef_pos_2_init, eef_pos_1, eef_pos_2, tray_rot_init)
+            tray_site_1 = self.data.site_xpos[self.model.site(name="tray_site_1").id]
+            tray_site_2 = self.data.site_xpos[self.model.site(name="tray_site_2").id]
+            tray_rot = turn_quat(tray_site_1, tray_site_2, eef_pos_1, eef_pos_2, tray_rot_init)
             self.data.mocap_quat[self.model.body_mocapid[self.model.body(name='tray_mocap').id]] = tray_rot
             # print(tray_pos, tray_rot_init, tray_rot, flush=True)
 
             
-        self.eef_pos_1_init = self.data.site_xpos[self.planner.tcp_id_1].copy()
-        self.eef_pos_2_init = self.data.site_xpos[self.planner.tcp_id_2].copy()
+        # self.eef_pos_1_init = self.data.site_xpos[self.planner.tcp_id_1].copy()
+        # self.eef_pos_2_init = self.data.site_xpos[self.planner.tcp_id_2].copy()
         if self.use_hardware:
             # Get current state
             current_pos_1 = np.array(self.rtde_r_1.getActualQ())

real_demo/real_demo/visualizer.py
@@ -87,6 +87,8 @@ class Visualizer(Node):
         self.data = mujoco.MjData(self.model)
 
         self.data.qpos[self.joint_mask_pos] = self.init_joint_position
+        self.init_tray_pos = self.data.mocap_pos[self.model.body_mocapid[self.model.body(name='tray_mocap').id]].copy()
+        self.init_tray_quat = self.data.mocap_quat[self.model.body_mocapid[self.model.body(name='tray_mocap').id]].copy()
 
         self.viewer = mujoco.viewer.launch_passive(self.model, self.data)
         self.viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = True
@@ -153,6 +155,8 @@ class Visualizer(Node):
 
             self.eef_pos_1_init = self.data.site_xpos[self.tcp_id_1].copy()
             self.eef_pos_2_init = self.data.site_xpos[self.tcp_id_2].copy()
+            self.tray_site_1_init = self.data.site_xpos[self.model.site(name="tray_site_1").id].copy()
+            self.tray_site_2_init = self.data.site_xpos[self.model.site(name="tray_site_2").id].copy()
 
             self.step_idx = 0
             self.timer = self.create_timer(self.model.opt.timestep, self.view_playback)
@@ -210,7 +214,13 @@ class Visualizer(Node):
             self.data.mocap_pos[self.model.body_mocapid[self.model.body(name='tray_mocap').id]] = tray_pos
 
             tray_rot_init = self.data.mocap_quat[self.model.body_mocapid[self.model.body(name='tray_mocap').id]]
-            tray_rot = turn_quat(self.eef_pos_1_init, self.eef_pos_2_init, eef_pos_1, eef_pos_2, tray_rot_init)
+
+            tray_site_1 = self.data.site_xpos[self.model.site(name="tray_site_1").id].copy()
+            tray_site_2 = self.data.site_xpos[self.model.site(name="tray_site_2").id].copy()
+
+            print(tray_site_1, eef_pos_1, flush=True)
+
+            tray_rot = turn_quat(tray_site_2, tray_site_1, eef_pos_1, eef_pos_2, tray_rot_init)
             self.data.mocap_quat[self.model.body_mocapid[self.model.body(name='tray_mocap').id]] = tray_rot
 
             print(tray_pos, tray_rot_init, tray_rot, flush=True)
@@ -230,6 +240,8 @@ class Visualizer(Node):
             self.step_idx = 0
             self.data.qpos[self.joint_mask_pos] = self.init_joint_position
             self.data.qvel[self.joint_mask_vel] = np.zeros(self.init_joint_position.shape)
+            self.data.mocap_pos[self.model.body_mocapid[self.model.body(name='tray_mocap').id]] = self.init_tray_pos
+            self.data.mocap_quat[self.model.body_mocapid[self.model.body(name='tray_mocap').id]] = self.init_tray_quat
             mujoco.mj_step(self.model, self.data)
             self.viewer.sync()
 

real_demo/sampling_based_planner/mjx_planner.py
@@ -173,8 +173,10 @@ class cem_planner():
 
 		self.hande_id_2 = self.model.body(name="hande_2").id
 		self.tcp_id_2 = self.model.site(name="tcp_2").id
+		self.tray_site_1_id = self.model.site(name="tray_site_1").id
+		self.tray_site_2_id = self.model.site(name="tray_site_2").id
 
-		self.tray_idx = self.model.jnt_qposadr[self.model.body_jntadr[self.model.body(name="tray").id]]
+		# self.tray_idx = self.model.jnt_qposadr[self.model.body_jntadr[self.model.body(name="tray").id]]
 
 		self.weld_id_1 = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_EQUALITY, "grasp_1")
 		self.weld_id_2 = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_EQUALITY, "grasp_2")
@@ -398,19 +400,12 @@ class cem_planner():
 		v1 = jnp.array([p2[0] - p1[0], p2[1] - p1[1]]) 
 		v2 = jnp.array([p4[0] - p3[0], p4[1] - p3[1]]) 
 
-		# Calculate the dot product 
-		dot_product = jnp.dot(v1, v2) 
+		angle1 = jnp.arctan2(v1[1], v1[0])
+		angle2 = jnp.arctan2(v2[1], v2[0])
 
-		# Calculate the cosine of the angle 
-		cos_angle = dot_product / (jnp.linalg.norm(v1)  * jnp.linalg.norm(v2) ) 
-		
-		# Ensure the value is within the valid domain for arccos to avoid floating point errors
-		cos_angle = jnp.clip(cos_angle, -1.0, 1.0)
+		angle_rad = angle2 - angle1
 
-		# Calculate the angle in radians and convert to degrees 
-		angle_deg = jnp.degrees(jnp.arccos(cos_angle) ) 
-
-		return angle_deg 
+		return jnp.degrees(angle_rad)  
 	
 	def quaternion_distance(self, q1, q2):
 		dot_product = jnp.abs(jnp.dot(q1, q2))
@@ -461,8 +456,8 @@ class cem_planner():
 
 	@partial(jax.jit, static_argnums=(0,))
 	def mjx_step(self, mjx_data, thetadot_single):
-		eef_pos_1_init = mjx_data.site_xpos[self.tcp_id]
-		eef_pos_2_init = mjx_data.site_xpos[self.tcp_id_2]
+		# eef_pos_1_init = mjx_data.site_xpos[self.tcp_id]
+		# eef_pos_2_init = mjx_data.site_xpos[self.tcp_id_2]
 	
 		qvel = mjx_data.qvel.at[self.joint_mask_vel].set(thetadot_single)
 		mjx_data = mjx_data.replace(qvel=qvel)
@@ -489,7 +484,9 @@ class cem_planner():
 		# Set tray position and orientation
 		tray_pos = (eef_pos_1+eef_pos_2)/2	
 		tray_rot_init = mjx_data.mocap_quat[self.tray_mocap_idx]
-		tray_rot = self.turn_tray(eef_pos_1_init, eef_pos_2_init, eef_pos_1, eef_pos_2, tray_rot_init)
+		tray_site_1 = mjx_data.site_xpos[self.tray_site_1_id]
+		tray_site_2 = mjx_data.site_xpos[self.tray_site_2_id]
+		tray_rot = self.turn_tray(tray_site_1, tray_site_2, eef_pos_1, eef_pos_2, tray_rot_init)
 
 		# tray_rot = jnp.array([1, 0, 0, 0])
 
@@ -624,7 +621,12 @@ class cem_planner():
 			cost_weights['move']*cost_weights['orientation']*cost_r_tray
 		)	
 
-		cost_list = jnp.array([cost_c, cost_weights['move']*cost_weights['distance']*cost_dist, cost_weights['pick']*cost_weights['position']*cost_g, cost_weights['pick']*cost_weights['orientation']*cost_r])
+		cost_list = jnp.array([
+			cost_c, 
+			cost_weights['move']*cost_weights['distance']*cost_dist, 
+			cost_weights['pick']*cost_weights['position']*cost_g+cost_weights['move']*cost_weights['position']*cost_g_tray , 
+			cost_weights['pick']*cost_weights['orientation']*cost_r+cost_weights['move']*cost_weights['orientation']*cost_r_tray
+		])
 
 		return cost, cost_list
 	

real_demo/sampling_based_planner/mpc_planner.py
@@ -250,7 +250,8 @@ class run_cem_planner:
         )
 
         # Get mean velocity command (average middle 80% of trajectory)
-        thetadot_cem = np.mean(thetadot_horizon[1:int(self.num_steps*0.9)], axis=0)
+        # thetadot_cem = thetadot_horizon[1]
+        thetadot_cem = np.mean(thetadot_horizon[1:int(self.num_steps*0.7)], axis=0)
 
         thetadot_1 = thetadot_cem[:6]
         thetadot_2 = thetadot_cem[6:]
@@ -268,20 +269,20 @@ class run_cem_planner:
             joint_states[self.cem.joint_mask_pos] = current_pos
 
             # Arm 1 control
-            # if current_cost_g_1 < self.ik_pos_thresh and current_cost_r_1 < self.ik_rot_thresh:
-            #     print("Activated ik solution for arm 1")
-            #     ik_solver_1 = InverseKinematicsSolver(
-            #         self.model, joint_states, "tcp")
-            #     ik_solver_1.set_target(self.target_pos_1, self.target_rot_1)
-            #     thetadot_1 = ik_solver_1.solve(dt=self.collision_free_ik_dt)[self.cem.joint_mask_vel][:self.num_dof//2]
+            if current_cost_g_1 < self.ik_pos_thresh and current_cost_r_1 < self.ik_rot_thresh:
+                print("Activated ik solution for arm 1")
+                ik_solver_1 = InverseKinematicsSolver(
+                    self.model, joint_states, "tcp")
+                ik_solver_1.set_target(self.target_pos_1, self.target_rot_1)
+                thetadot_1 = ik_solver_1.solve(dt=self.collision_free_ik_dt)[self.cem.joint_mask_vel][:self.num_dof//2]
             
-            # # Arm 2 control
-            # if current_cost_g_2 < self.ik_pos_thresh and current_cost_r_2 < self.ik_rot_thresh:
-            #     print("Activated ik solution for arm 2")
-            #     ik_solver_2 = InverseKinematicsSolver(
-            #         self.model, joint_states, "tcp_2")
-            #     ik_solver_2.set_target(self.target_pos_2, self.target_rot_2)
-            #     thetadot_2 = ik_solver_2.solve(dt=self.collision_free_ik_dt)[self.cem.joint_mask_vel][:self.num_dof//2]
+            # Arm 2 control
+            if current_cost_g_2 < self.ik_pos_thresh and current_cost_r_2 < self.ik_rot_thresh:
+                print("Activated ik solution for arm 2")
+                ik_solver_2 = InverseKinematicsSolver(
+                    self.model, joint_states, "tcp_2")
+                ik_solver_2.set_target(self.target_pos_2, self.target_rot_2)
+                thetadot_2 = ik_solver_2.solve(dt=self.collision_free_ik_dt)[self.cem.joint_mask_vel][self.num_dof//2:]
 
         # Combine control commands
         thetadot = np.concatenate((thetadot_1, thetadot_2))